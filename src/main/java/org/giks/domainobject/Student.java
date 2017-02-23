3b1cd8dd8a6fdaaf06027a7ce1731929437a4757
Dinesh <dpun.dinesh@gmail.com>
2017-02-23 09:50:16 +0530

    Merge branch 'master' of https://github.com/GiksRepo/NewAdwork
    # Conflicts:
    #	target/m2e-wtp/web-resources/META-INF/MANIFEST.MF

---
+++ src/main/java/org/giks/domainobject/Student.java
@@ -0,0 +1,111 @@
+package org.giks.domainobject;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "student")
+public class Student implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name = "admission_no")
+	private Long admissionNo;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "middle_name")
+	private String middleName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "father_name")
+	private String fatherName;
+	
+	@Column(name = "address")
+	private String address;
+	
+	@Column(name = "date_of_birth")
+	private String dateOfBirth;
+	
+	@Column(name = "section")
+	private String section;
+	
+	public Long getAdmissionNo() {
+		return admissionNo;
+	}
+
+	public void setAdmissionNo(Long admissionNo) {
+		this.admissionNo = admissionNo;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFatherName() {
+		return fatherName;
+	}
+
+	public void setFatherName(String fatherName) {
+		this.fatherName = fatherName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getSection() {
+		return section;
+	}
+
+	public void setSection(String section) {
+		this.section = section;
+	}
+
+	public Student() {
+		// TODO Auto-generated constructor stub
+	}
+}
